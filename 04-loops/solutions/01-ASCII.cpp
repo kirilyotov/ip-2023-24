1fbf5ba4045d4a5a87f7c6fbcf0ee98f7480225a
Dako Dimov <dako98@gmail.com>
2023-12-22 14:47:27 +0200

    Removed unused library. Fixed new line in output.

--- 04-loops/solutions/01-ASCII.cpp
+++ 04-loops/solutions/01-ASCII.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <iomanip>
 
 int main()
 {
 
-  std::cout << INT8_MAX;
+  std::cout << INT8_MAX << std::endl;
   for (unsigned char ch = 0; ch <= INT8_MAX; ++ch)
   {
     std::cout << (int)ch << " -> " << ch << std::endl;
