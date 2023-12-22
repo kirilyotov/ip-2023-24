93051eb157a96d4b5f14c4bba8a717b819441220
Dako Dimov <dako98@gmail.com>
2023-12-22 15:06:00 +0200

    Add check for triangle validity. Remove new line.

--- 05-functions/solutions/14-TriangleTypeByCoordinates.cpp
+++ 05-functions/solutions/14-TriangleTypeByCoordinates.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 
+const float EPS = 0.001f;
+
 double distance(double x1, double y1, double x2, double y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
@@ -10,8 +12,14 @@ void triangleType(double x1, double y1, double x2, double y2, double x3, double 
     double b = distance(x2, y2, x3, y3);
     double c = distance(x3, y3, x1, y1);
 
-    if (a == b && b == c){
-     std::cout << "Equilateral\n";
+    if( a < EPS || b < EPS || c < EPS ||
+        fabs(a + b - c) < EPS || 
+        fabs(a + c - b) < EPS ||
+        fabs(b + c - a) < EPS ){
+     std::cout << "Invalid";
+    }
+    else if (a == b && b == c){
+     std::cout << "Equilateral";
     }
     else if (a == b || b == c || c == a){
     std::cout << "Isosceles";
