c4fc34a0a0fe28467119053f5cef0b71b6557536
Dako Dimov <dako98@gmail.com>
2024-01-09 10:49:05 +0200

    Include original solution. Move optimized solution in separate function.

--- 06-Arrays/solutions/13-Rotations.cpp
+++ 06-Arrays/solutions/13-Rotations.cpp
@@ -10,6 +10,35 @@ void readArray(int arr[], int &n, int &rot)
     std::cin >> rot;
 }
 
+void rotateArray(int arr[], int n, int rot)
+{
+    if (rot > 0)
+    {
+        for (int r = 0; r < rot; r++)
+        {
+            int last = arr[n - 1];
+            for (int i = n - 1; i > 0; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = last;
+        }
+    }
+    else
+    {
+        rot = std::abs(rot);
+        for (int r = 0; r < rot; r++)
+        {
+            int first = arr[0];
+            for (int i = 0; i < n - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            arr[n - 1] = first;
+        }
+    }
+}
+
 void reverseArr(int arr[], int startIdx, int endIdx)
 {
     for(; startIdx < endIdx; startIdx++,endIdx--)
@@ -18,7 +47,7 @@ void reverseArr(int arr[], int startIdx, int endIdx)
     }
 }
 
-void rotateArray(int arr[], int size, int rot)
+void quickRotateArray(int arr[], int size, int rot)
 {
     rot = rot < 0 ? size - (-rot % size) : rot % size;
 
