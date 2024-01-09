4487fc3e0ec7986abe3600be7b370dd10d9930a1
Dako Dimov <dako98@gmail.com>
2024-01-09 10:29:31 +0200

    Move epsilon check to a separate function. Fix validity check. Fix equality checks. Optimize power 2.

--- 05-functions/solutions/14-TriangleTypeByCoordinates.cpp
+++ 05-functions/solutions/14-TriangleTypeByCoordinates.cpp
@@ -3,8 +3,13 @@
 
 const float EPS = 0.001f;
 
+bool isEqual(double num1, double num2, double precision)
+{   
+    return abs(num1 - num2) < precision;
+}
+
 double distance(double x1, double y1, double x2, double y2) {
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    return sqrt((x2 - x1)*(x2 - x1)) + (y2 - y1)*(y2 - y1);
 }
 
 void triangleType(double x1, double y1, double x2, double y2, double x3, double y3) {
@@ -12,16 +17,14 @@ void triangleType(double x1, double y1, double x2, double y2, double x3, double 
     double b = distance(x2, y2, x3, y3);
     double c = distance(x3, y3, x1, y1);
 
-    if( a < EPS || b < EPS || c < EPS ||
-        fabs(a + b - c) < EPS || 
-        fabs(a + c - b) < EPS ||
-        fabs(b + c - a) < EPS ){
+    if( isEqual(a, 0, EPS) || isEqual(b, 0, EPS) || isEqual(c, 0, EPS) ||
+        a + b < c || a + c < b || b + c < a ){
      std::cout << "Invalid";
     }
-    else if (a == b && b == c){
+    else if (isEqual(a, b, EPS) && isEqual(b, c, EPS)){
      std::cout << "Equilateral";
     }
-    else if (a == b || b == c || c == a){
+    else if (isEqual(a, b, EPS) || isEqual(b, c, EPS) || isEqual(c, a, EPS)){
     std::cout << "Isosceles";
     }
     else{
